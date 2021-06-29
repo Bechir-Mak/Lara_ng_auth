9758aa2af7a4dcc27b09c2dd6869be5d8938f87d
Bechir_Mak <bechir.makni1@gmail.com>
2021-06-29 19:32:47 +0200

    auth

---
+++ backend/app/Http/Middleware/CORS.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CORS
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+
+        header('Access-Control-Allow-Origin', '*');
+        header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PUT');
+
+
+
+        return $next($request);
+    }
+}
